858add15038c5dfeb3631f861f72aaae36bf15c4
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-07-29 18:57:37 +0200

    feedback/fixed

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,26 +1,19 @@
 import PropTypes from 'prop-types';
 import { Box } from 'components/Box';
+import { nanoid } from 'nanoid';
 
 import { Button, Item } from './FeedbackOptions.styled';
 
 export default function FeedbackOptions({ onLeaveFeedback, options }) {
   return (
     <Box display="flex" ml={[4]} p={[0]} position="relative" as="ul">
-      <Item>
-        <Button type="button" onClick={() => onLeaveFeedback(options[0])}>
-          Good
-        </Button>
-      </Item>
-      <Item>
-        <Button type="button" onClick={() => onLeaveFeedback(options[1])}>
-          Neutral
-        </Button>
-      </Item>
-      <Item>
-        <Button type="button" onClick={() => onLeaveFeedback(options[2])}>
-          Bad
-        </Button>
-      </Item>
+      {options.map(option => (
+        <Item key={nanoid()}>
+          <Button type="button" onClick={() => onLeaveFeedback(option)}>
+            {option}
+          </Button>
+        </Item>
+      ))}
     </Box>
   );
 }
